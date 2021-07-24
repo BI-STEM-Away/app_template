a340fc80da3786e9a3c28bd536b1a03775f7367c
Samuel Bharti <samuelbharti@gmail.com>
2021-07-24 18:24:18 +0530

    Update global.R

--- global.R
+++ global.R
@@ -2,15 +2,7 @@
 # Global.R is run one time at app initiallization. 
 library(bs4Dash)
 library(shiny)
-library(shinyWidgets)
-library(ggplot2)
-library(Cairo) 
-library(hrbrthemes)
 library(fresh)
-library(plotly)
-library(DT)
-library(sortable)
-library(dplyr)
 
 #Import Functions from External R scripts.
 
